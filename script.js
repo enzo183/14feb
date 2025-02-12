978f29754f584106fed8b6a918c6255aa2c3f265
enzo183 <47628558+enzo183@users.noreply.github.com>
2025-02-12 15:38:49 +0100

    Croise les doigts

--- script.js
+++ script.js
@@ -46,15 +46,27 @@ const messages = [
 
 let messageIndex = 0;
 
-function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
+
+}
+document.addEventListener('DOMContentLoaded', function() {
+    let noButtonClicked = false;
+
+    document.getElementById('.no-button').addEventListener('click', function() {
+        noButtonClicked = true;
+       const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
-}
+    });
 
-function handleYesClick() {
-    window.location.href = "yes_page.html";
-}
+    document.getElementById('yesButton').addEventListener('click', function() {
+        if (noButtonClicked) {
+            // Rediriger vers une page si "Non" a été cliqué auparavant
+            window.location.href = "yes_page.html";
+        } else {
+            // Rediriger vers une autre page si "Oui" est cliqué directement
+            window.location.href = 'direct_yes_page.html';
+        }
+    });
